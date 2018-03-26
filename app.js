e1b411bf1e99841241ac7f89921945215eddfc14
Accalia de Elementia <accalia.de.elementia@gmail.com>
2018-03-26 16:25:02 +0000

    initial download and delete script.

---
+++ app.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const request = require('request');
+
+const fs = require('fs-extra');
+const path = require('path');
+
+
+const makePromiseRequest = (config) => new Promise((resolve, reject) => {
+        return request(config, (err, _, res) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(res);
+        });
+    })
+    .then(data => JSON.parse(data));
+
+const getChannels = () => makePromiseRequest({
+        method: 'POST',
+        url: 'https://slack.com/api/channels.list',
+        formData: {
+            token: args.token,
+            exclude_members: 'true',
+            count: 500
+        }
+    })
+    .then(data => {
+        const res = {};
+        data.channels.forEach(entry => res[entry.id] = entry.name);
+        return res;
+    });
+
+const getUsers = () => makePromiseRequest({
+        method: 'POST',
+        url: 'https://slack.com/api/users.list',
+        formData: {
+            token: args.token,
+            count: 500
+        }
+    })
+    .then(data => {
+        const res = {};
+        data.members.forEach(entry => res[entry.id] = entry.name);
+        return res;
+    });
+
+const getFiles = (users, channels) => makePromiseRequest({
+    method: 'POST',
+    url: 'https://slack.com/api/files.list',
+    formData: {
+        token: args.token,
+        ts_from: 0,
+        ts_to: Math.floor(Date.now() / 1000) - args.cutoffDays * 24 * 60 * 60,
+        count: 50,
+        page: 1
+    }
+}).then(data => data.files
+    .filter(file => file.channels[0])
+    .map(record => {
+        return {
+            filename: `${new Date(record.timestamp * 1000).toISOString().slice(0,-5)} - ${users[record.user]} - ${record.name}`,
+            folder: channels[record.channels[0]],
+            permalink: record.url_private_download,
+            id: record.id,
+        };
+    }));
+
+const deleteFile = file => {
+    const folder = path.join(args.destination, file.folder);
+    const dest = path.join(folder, file.filename);
+    console.log(`Downloading \`${file.filename}\` from \`${file.folder}\``);
+    if (!args.runDelete) {
+        return Promise.resolve();
+    }
+    return fs.ensureDir(folder)
+        .then(() => new Promise((res, rej) => {
+            request({
+                    url: file.permalink,
+                    headers: {
+                        Authorization: `Bearer ${args.token}`
+                    }
+                })
+                .pipe(fs.createWriteStream(dest))
+                .on('error', (err) => rej(err))
+                .on('close', res());
+        })).then(() => makePromiseRequest({
+            url: 'https://slack.com/api/files.delete',
+            formData: {
+                token: args.token,
+                file: file.id
+            }
+        }));
+};
+
+const deleteFiles = files => Promise.all(files.map(file => deleteFile(file)));
+
+const args = yargs
+    .describe('token', 'Slack API token for authentication')
+    .default('token', process.env.SLACK_TOKEN)
+    .describe('dry-run', 'Perform a dry run')
+    .describe('run-delete', 'Download and delete files from Slack')
+    .conflicts('dry-run', 'run-delete')
+    .alias('destination', 'dest')
+    .describe('destination', 'Download File Destination')
+    .default('destination', '.')
+    .coerce('destination', path.resolve)
+    .describe('cutoff-days', 'Number of days to preserve files for')
+    .default('cutoff-days', 120)
+    .number('cutoff-days')
+    .argv;
+
+Promise.all([getUsers(), getChannels()])
+    .then(([users, channels]) => getFiles(users, channels))
+    .then(deleteFiles)
+    .then(() => console.log('DONE'));
