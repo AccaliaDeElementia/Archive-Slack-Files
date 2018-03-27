378e8ca8665dba9d9b99a254093ae5adea0ca816
Accalia de Elementia <accalia.de.elementia@gmail.com>
2018-03-27 13:26:10 +0000

    search deeper for files

--- app.js
+++ app.js
@@ -46,27 +46,39 @@ const getUsers = () => makePromiseRequest({
         return res;
     });
 
-const getFiles = (users, channels) => makePromiseRequest({
-    method: 'POST',
-    url: 'https://slack.com/api/files.list',
-    formData: {
-        token: args.token,
-        ts_from: 0,
-        ts_to: Math.floor(Date.now() / 1000) - args.cutoffDays * 24 * 60 * 60,
-        count: 50,
-        page: 1
-    }
-}).then(data => data.files
-    .filter(file => file.channels[0])
-    .map(record => {
-        return {
-            filename: `${new Date(record.timestamp * 1000).toISOString().slice(0,-5)} - ${users[record.user]} - ${record.name}`.replace(/[\/\\:]/g, '_'),
-            folder: channels[record.channels[0]],
-            permalink: record.url_private_download,
-            id: record.id,
-        };
-    })
-    .filter(file => file.permalink));
+const getFiles = (users, channels) => {
+    const getem = (page) => makePromiseRequest({
+        method: 'POST',
+        url: 'https://slack.com/api/files.list',
+        formData: {
+            token: args.token,
+            ts_from: 0,
+            ts_to: Math.floor(Date.now() / 1000) - args.cutoffDays * 24 * 60 * 60,
+            count: 50,
+            page
+        }
+    }).then(data => data.files
+        .filter(file => file.channels[0])
+        .map(record => {
+            return {
+                filename: `${new Date(record.timestamp * 1000).toISOString().slice(0,-5)} - ${users[record.user]} - ${record.name}`.replace(/[\/\\:]/g, '_'),
+                folder: channels[record.channels[0]],
+                permalink: record.url_private_download,
+                id: record.id,
+            };
+        })
+        .filter(file => file.permalink));
+    const getUntil = (i, results, count) => {
+        return getem(i).then(files => {
+            results = results.concat(files);
+            if (results.length >= count) {
+                return Promise.resolve(results);
+            }
+            return getUntil(i + 1, results, count);
+        });
+    };
+    return getUntil(1, [], 50);
+};
 
 const deleteFile = file => {
     const folder = path.join(args.destination, file.folder);
@@ -87,7 +99,7 @@ const deleteFile = file => {
                 .on('error', (err) => rej(err))
                 .on('close', res());
         }))
-        .then(()=>console.log('Download complete!'))
+        .then(() => console.log('Download complete!'))
         .then(() => makePromiseRequest({
             method: 'POST',
             url: 'https://slack.com/api/files.delete',
@@ -102,12 +114,12 @@ const deleteFiles = files => {
     const _files = files.slice();
     const next = () => {
         const file = _files.pop();
-        if (!file){
+        if (!file) {
             return Promise.resolve();
         }
         return deleteFile(file).then(next);
     };
-    return next().catch(e=>console.error(`Whoopsie! ${e.message}`));
+    return next().catch(e => console.error(`Whoopsie! ${e.message}`));
 };
 
 const args = yargs
